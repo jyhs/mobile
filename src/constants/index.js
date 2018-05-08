cb66e0c2c3afe181b7fe373c0d278a8e06a10674
张海斌 <tony@zhanghaibindeMacBook-Pro.local>
2018-05-08 21:22:10 +0800

    update

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const ApiBasePath = 'http://www.coral123.com:41433';
 export const ApiBasePathBack = 'https://111.231.136.250:1433';
-export const ImageBasePath = 'http://www.coral123.com:41433/image/uploads';
-export const SmallImageBasePath = 'http://www.coral123.com:41433/image/uploads/small';
-export const AvatarBasePath = 'http://www.coral123.com:41433/image/user/';
+export const ImageBasePath = 'http://www.coral123.com/image/uploads';
+export const SmallImageBasePath = 'http://www.coral123.com/image/uploads/small';
+export const AvatarBasePath = 'http://www.coral123.com/image/user/';
