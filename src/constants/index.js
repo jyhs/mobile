b7ceaf7530a813d7ba685efa5e54e9dbfa441ddc
fdjiangwu <fdjiangwu@yeah.net>
2018-05-15 23:26:21 +0800

    切到250

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const ApiBasePath = 'http://www.coral123.com:41433';
-export const ImageBasePath = 'http://www.coral123.com/image/uploads';
+export const ApiBasePath = 'http://111.231.136.250:1433';
+export const ImageBasePath = 'http://111.231.136.250/image/uploads';
 export const SmallImageBasePath = 'http://111.231.136.250:1433/api/material/image/base64/small';
 export const AvatarBasePath = 'http://111.231.136.250:1433/api/users/get/logo';
